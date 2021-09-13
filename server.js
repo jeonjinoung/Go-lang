aca125fb748e781f5677df55f95bd914bf022ab1
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2021-09-14 08:51:28 +0900

    세번째 서버 연습하는파일

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+//Post 요청 = 댓글작성 or 글작성
+//http://127.0.0.1:5500/Post.html
+
+
 const express = require('express');
 const app = express();
 
-app.listen(6400,function(){
-    console.log('listening on 6400');
+app.listen(8080,function(){
+    console.log('listening on 8080');
+});
+//누군가가 /pet으로 방문을 하면.. pet관련된 안내문을 띄어주자
+app.get('/login', function(req, res){
+    res.send('로그인 페이지 입니다..');
 });
 
- app.get('/menu', function(req, res){
-     res.send('메뉴페이지입니다...');
- });
-
- app.get('/', function(req, res){
-     res.sendFile(__dirname + '/blogfollow.html')
- });
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/Post.html')
+});
