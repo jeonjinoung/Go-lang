8010398908800f0ff23b192200472790d11e2b8f
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2021-09-14 16:47:52 +0900

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const togglebtn = document.querySelector('.navbar__tooglebtn');const togglebtn = document.querySelector('.navbar__tooglebtn');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
-
-togglebtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    icons.classList.toggle('active');
-})
